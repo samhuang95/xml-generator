bfc94936a04b3e078bddfbf4bd3f8eb0337073a2
samhuang95 <sam.huang.veda@gmail.com>
2025-06-27 02:27:26 +0800

    Modify the builder code

--- builder.ps1
+++ builder.ps1
@@ -1,8 +1,1 @@
-pyinstaller `
-  --onefile `
-  --name xml-generator `
-  --add-data "src\models;src\models" `
-  --add-data "src\routes;src\routes" `
-  --add-data "src\static;src\static" `
-  --add-data "src\database;src\database" `
-  src\main.py
+pyinstaller app.spec
